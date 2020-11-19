a4d81b929693e210219412a93cc146897ff9c0b6
umarrg <umarabubakarfta@gmail.com>
2020-11-19 17:25:27 +0100

    done with dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,26 +16,31 @@ void main() {
   );
 }
 
-
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
-    int leftDiceNumber = 1;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      print('diceNumber = $rightDiceNumber');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-   
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-               setState(() {
-                 leftDiceNumber = 5;
-               });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -42,9 +48,9 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right buttom got pressed');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           )
         ],
