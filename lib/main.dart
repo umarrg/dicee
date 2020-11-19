6ff69827400ffc3b55ae357c27776079123af630
umarrg <umarabubakarfta@gmail.com>
2020-11-19 16:40:10 +0100

    add statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+    int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(child: 
-    Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        )
-      ],
-    ),
-  );
+   
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+               setState(() {
+                 leftDiceNumber = 5;
+               });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right buttom got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
